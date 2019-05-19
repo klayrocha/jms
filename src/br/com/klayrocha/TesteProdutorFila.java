ea3fcc6ed002e5b42189541c16b7536c198665dd
Francis Klay Rocha <klayrocha@Macs-MBP.lan>
2019-05-19 22:36:59 +0100

    first commit

---
+++ src/br/com/klayrocha/TesteProdutorFila.java
@@ -0,0 +1,36 @@
+package br.com.klayrocha;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
+public class TesteProdutorFila {
+
+	public static void main(String[] args) throws Exception {
+		
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = factory.createConnection();
+		connection.start();
+		
+		Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+		
+		Destination fila = (Destination) context.lookup("financeiro");
+		
+		MessageProducer producer = session.createProducer(fila);
+		for (int i = 1; i < 2000; i++) {
+			Message message = session.createTextMessage("<pedido><id>"+i+"</id></pedido>");
+			producer.send(message);
+		}
+		//new Scanner(System.in).nextLine();
+		
+		session.close();
+		connection.close();
+		context.close();
+	}
+
+}
